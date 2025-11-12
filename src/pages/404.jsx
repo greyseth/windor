1b717a44a5d92ffd09aad7c57cff35e4f981542d
greyseth <anargya2gilland@gmail.com>
2025-11-12 12:17:56 +0700

    404 Page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,21 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 export default function NotFound() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
     <>
-      <p>Page URL not found</p>
+      <div className="w-full h-full flex flex-col justify-center items-center gap-8 text-center">
+        <h1 className="text-3xl font-bold">404</h1>
+        <p className="font-bold">
+          The URL at {location.pathname} was not found. Make sure the path is
+          correct
+        </p>
+        <button className="btn primary" onClick={() => navigate("/app")}>
+          Back to Home
+        </button>
+      </div>
     </>
   );
 }
